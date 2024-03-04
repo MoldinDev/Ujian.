cf3d8f4b229cd06141c7cc585e52354e10bf3b71
MoldinDev <154769605+MoldinDev@users.noreply.github.com>
2024-03-04 21:30:16 +0700

    Update layout.jsx

--- src/app/(no-layout)/[paketsoal]/mulaiPaket1/soal/layout.jsx
+++ src/app/(no-layout)/[paketsoal]/mulaiPaket1/soal/layout.jsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-        <div className="lg:ms-[300px] md:ms-[250px] mt-[125px]">
+        <div className="lg:ms-[300px] md:ms-[250px] mt-[130px]">
         <marquee className="fixed top-0 text-[#23232393]">Made by love, Maulana Syafiuddin. Untuk saran dan masukan hubungi Instagram: <span className='text-lime-500'>@maulanasyafiuddin</span></marquee>
         {children}
         </div>
